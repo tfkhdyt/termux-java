bd71e6def5403e3d4903cfd39330247bc0aff1fd
tfkhdyt <ta142fa@gmail.com>
2021-02-12 07:29:39 +0700

    Memperbaiki installer

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
+clear
 pkg install ecj dx termux-tools
 mv java /data/data/com.termux/files/usr/bin/ 
+clear
+echo "Termux Java berhasil di-install!"
+echo "Nama file harus sama dengan nama class agar tidak terjadi error"
