fc6becbc05587f647c59efa0f3041b151b108027
tfkhdyt <ta142fa@gmail.com>
2021-05-23 10:29:03 +0700

    fix installer

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 clear
 pkg install ecj dx termux-tools
-cp java $PRERIX/bin/ 
+cp jv $PRERIX/bin/ 
 clear
 echo "Termux Java berhasil di-install!"
 echo "Nama file harus sama dengan nama class agar tidak terjadi error"
